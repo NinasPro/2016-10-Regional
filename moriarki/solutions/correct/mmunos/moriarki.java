d58e552f0e2c9c1a219d97451cf5217f25f8b879
Martin Munoz <mmunos@uc.cl>
2016-09-20 02:00:47 -0300

    cambio en subtarea 2 y solucion en java añadida

---
+++ moriarki/solutions/correct/mmunos/moriarki.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class moriarki {
+
+	public static void main(String[] args) {
+		Scanner seer = new Scanner(System.in);
+		int res = 0;
+		int n = seer.nextInt();
+		int curr = seer.nextInt();
+		for(int i = 1; i < n; i++){
+			int c = seer.nextInt();
+			int k = seer.nextInt();
+			if(c == 1){
+				curr *= k;
+			}
+			else{
+				res += curr;
+				curr = k;
+			}
+		}
+		System.out.println(res + curr);
+	}
+
+}
