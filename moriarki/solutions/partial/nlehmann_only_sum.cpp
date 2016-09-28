226ab7228afd56a90045e1f84adcea7d0eb6a1a9
Nico Lehmann <nlehmann@dcc.uchile.cl>
2016-09-28 14:06:35 +0900

    Normalizing input and adding expected files for moriarki

---
+++ moriarki/solutions/partial/nlehmann_only_sum.cpp
@@ -0,0 +1,28 @@
+/*
+ * -----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <nlehmann@dcc.uchile.cl> wrote this file. As long as you retain this notice
+ * you can do whatever you want with this stuff. If we meet some day, and you
+ * think this stuff is worth it, you can buy me a beer in return Nicolás Lehmann
+ * -----------------------------------------------------------------------------
+ */
+
+#include <cstdio>
+using namespace std;
+
+
+int main () {
+  int N;
+  scanf("%d", &N);
+
+  int count = 0;
+  for (int i = 0; i < N; ++i) {
+    int a;
+    scanf("%d", &a);
+    count += a;
+    if (i < N - 1)
+      scanf("%d", &a);
+  }
+  printf("%d\n", count);
+}
+
